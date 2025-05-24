aa3eb260a73e418e379118a2386460e686cb1017
MuriloPiresTavares23 <mriphonesmt@gmail.com>
2025-05-24 10:43:34 -0400

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,11 +1,12 @@
 // Substitua com seu config do Firebase
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyDsIWsBTtP7YiIYw0a1Reru3XFA9wajP9k",
+  authDomain: "mr-iphones-mt-catalogo.firebaseapp.com",
+  projectId: "mr-iphones-mt-catalogo",
+  storageBucket: "mr-iphones-mt-catalogo.firebasestorage.app",
+  messagingSenderId: "231517888492",
+  appId: "1:231517888492:web:b12539a232484248e52ac9",
+  measurementId: "G-4ZVCJET8BY"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
